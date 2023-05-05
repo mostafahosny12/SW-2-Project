3b4f65679475b4c207f69eebf96f6ce45eae16ec
mostafahosny12 <mostafahosny723@gmail.com>
2023-05-05 23:52:20 +0200

    Added dotenv

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+
+import {engine} from 'express-handlebars';
+
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.mongoConnectionUrl);
+
+const app = express();
+
+app.engine('handlebars' , engine());
+
+app.set('view engine' , 'handlebars');
+
+app.set('views' , './templates');
+
+app.listen(process.env.port , () => {
+    console.log('Started app on http://localhost:' + PORT);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
